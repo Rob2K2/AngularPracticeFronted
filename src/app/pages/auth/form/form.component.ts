b40ce85bc76075ccddd9484620012cd299ee4270
Rob2K2 <merino@live.com>
2022-05-23 14:19:30 -0400

    Project created - Routes created - Components created - Added bootstrap - Login / Sign up form created

---
+++ src/app/pages/auth/form/form.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ACTIONS } from '@shared/constants/constant';
+
+export interface OptionsForm {
+  id: string;
+  label: string;
+}
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent implements OnInit {
+  authForm!: FormGroup;
+  signIn = ACTIONS.signIn
+  @Input() options!: OptionsForm;
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    console.log('Save', this.authForm.value);
+  }
+
+  private initForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+}
